ed75dc9f6af91567c65aa678ead020d78515e2bf
Myo Thu Ko <myothuko@ucsy.edu.mm>
2022-12-08 22:53:13 +0630

    add RTK Query with multiple baseUrl (usage only with RTK Query)

--- 06_lesson_starter/src/index.js
+++ 06_lesson_starter/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root'))
-  .render(
-    <React.StrictMode>
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './features/todos/api/apiSlice';
+import { NewApiSlice } from './features/todos/api/newApiSlice';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ApiProvider api={[apiSlice, NewApiSlice]}>
       <App />
-    </React.StrictMode>
-  );
+    </ApiProvider>
+  </React.StrictMode>
+);
